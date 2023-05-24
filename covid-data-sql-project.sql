3777c37080cae2ac8f00ad54ea093c5d4ab7ad7b
MaryJay787 <josephmaryam.mj@gmail.com>
2023-05-24 09:02:00 -0400

    total_cases_and_population

--- covid-data-sql-project.sql
+++ covid-data-sql-project.sql
@@ -1,7 +1,16 @@
--- Query to view total cases vs. total deaths and aggregated the percentage out of 100. 
+-- Query to view total cases vs. total deaths and aggregated the percentage of the two. 
 SELECT
 location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 AS death_percentage
 FROM 
 `covid-data-387015.covid_data_exploration.covid_world_deaths` 
 WHERE 
 continent IS NOT NULL
+-- Query to view total cases vs. population and calculated the percentage of the two. 
+SELECT 
+location, date, total_cases, population, (total_cases/population)*100 AS contracted_pop_percent
+FROM
+`covid-data-387015.covid_data_exploration.covid_world_deaths`
+WHERE 
+location = 'United States' 
+ORDER BY
+1,2
